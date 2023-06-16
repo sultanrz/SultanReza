939c09d64932a9c0744230387651a2f0fb63ae41
Sultan Reza <sultanrezaok@gmail.com>
2023-06-16 21:55:10 +0700

    tambah

---
+++ app/src/main/java/com/example/jackprado/inputkaryawan.java
@@ -0,0 +1,14 @@
+package com.example.jackprado;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inputkaryawan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inputkaryawan);
+    }
+}
